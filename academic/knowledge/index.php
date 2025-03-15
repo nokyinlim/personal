3bd6374cd1d18a63f20d16d091c7ae3935c0c09c
Nok Yin Lim <157865784+nokyinlim@users.noreply.github.com>
2025-03-16 06:04:30 +0800

    Add Font Awesome stylesheet to knowledge index page

--- academic/knowledge/index.php
+++ academic/knowledge/index.php
@@ -9,6 +9,7 @@ include '../../components/cards.php'; ?>
   <title>Knowledge Organizers</title>
   <link rel="stylesheet" href="/style.css">
   <script src="/glow-effect.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
   <!-- Navigation -->
